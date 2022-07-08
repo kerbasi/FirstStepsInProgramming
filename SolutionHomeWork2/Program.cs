50146cad0c2b7f75d2c37588f2f5489efd002860
Kirill <kirill.novikov@gmail.com>
2022-07-08 22:59:49 +0300

    add solution for homework task 2

---
+++ SolutionHomeWork2/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите два числа:");
+string? inputOne = Console.ReadLine();
+string? inputTwo = Console.ReadLine();
+
+if (inputOne != null && inputTwo != null)
+{
+    int numOne = int.Parse(inputOne);
+    int numTwo = int.Parse(inputTwo);
+    Console.Write("Max = ");
+    if(numOne > numTwo) 
+    {
+        Console.Write(numOne);
+    }
+    else {
+        Console.Write(numTwo);
+    }
+}
