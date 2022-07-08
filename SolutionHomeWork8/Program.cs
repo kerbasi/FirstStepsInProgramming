5ceb15b3cc8cd15c25b63c7b3c172cc317fb571b
Kirill <kirill.novikov@gmail.com>
2022-07-08 23:43:19 +0300

    add solution for homework task 8

---
+++ SolutionHomeWork8/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Write("Введите номер : ");
+string? inputLine = Console.ReadLine();
+
+if (inputLine != null)
+{
+    int inputNum = int.Parse(inputLine);
+    if (inputNum > 2) {
+        string outputString = "2";
+
+        //int counter = 4;
+        // while(counter <= inputNum)
+        // {
+        //     outputString = outputString + ", " + counter;
+        //     counter += 2;
+        // }
+
+        for (int i = 4; i <= inputNum; i += 2) 
+        {
+            outputString = outputString + ", " + i;
+        }
+
+        Console.WriteLine(outputString);
+    }
+}
