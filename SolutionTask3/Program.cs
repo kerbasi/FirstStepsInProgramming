7f961be33a86f30761c51b9c29a51580ac7c5464
Kirill <kirill.novikov@gmail.com>
2022-07-08 21:37:19 +0300

    add solution for task 3

---
+++ SolutionTask3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите номер : ");
+string? inputLine = Console.ReadLine();
+
+if (inputLine != null)
+{
+    int inputNum = int.Parse(inputLine);
+    for (int i = - inputNum; i <= inputNum; i++) 
+    {
+        Console.WriteLine(i);
+    }
+}
