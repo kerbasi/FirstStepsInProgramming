c7324ae850001a39d0aca7d777a51a413da59a3d
Kirill <kirill.novikov@gmail.com>
2022-07-08 23:14:12 +0300

    add solution for homework task 6

---
+++ SolutionHomeWork6/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите номер : ");
+string? inputLine = Console.ReadLine();
+
+if (inputLine != null)
+{
+    int inputNum = int.Parse(inputLine);
+    if (inputNum % 2 == 1) {
+        Console.WriteLine("Не четное");
+    } else {
+        Console.WriteLine("Четное");
+    }
+}
