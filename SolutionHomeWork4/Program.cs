ad528fb29d3c9a33d91f23afe77276b9b3195a02
Kirill <kirill.novikov@gmail.com>
2022-07-08 23:06:43 +0300

    add solution for homework task 4

---
+++ SolutionHomeWork4/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Введите три числа:");
+string? inputOne = Console.ReadLine();
+string? inputTwo = Console.ReadLine();
+string? inputThree = Console.ReadLine();
+
+if (inputOne != null && inputTwo != null && inputThree != null)
+{
+    int max = int.Parse(inputOne);
+    int numTwo = int.Parse(inputTwo);
+    int numThree = int.Parse(inputThree);
+    Console.Write("Max = ");
+    if(numTwo > max) 
+    {
+        max = numTwo;
+    }
+    if(numThree > max) 
+    {
+        max = numThree;
+    }
+
+    Console.Write(max);
+}
