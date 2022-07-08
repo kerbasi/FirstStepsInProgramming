f7a84182cab3d0cfd04f5a2de96022538f8f0776
Kirill <kirill.novikov@gmail.com>
2022-07-08 21:00:38 +0300

    add solution for task 1

---
+++ SolutionTask1/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Введите два числа:");
+string inputOne = Console.ReadLine();
+string inputTwo = Console.ReadLine();
+
+if (inputOne != null && inputTwo != null)
+{
+    int numOne = int.Parse(inputOne);   
+    int numTwo = int.Parse(inputTwo);  
+
+    if (numOne == (int)Math.Pow(numTwo, 2)) 
+    {
+        Console.WriteLine("Yes");
+    } 
+    else 
+    {
+        Console.WriteLine("No");
+    }
+}
+
+
+
+
+
+
