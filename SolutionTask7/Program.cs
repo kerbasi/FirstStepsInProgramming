67e893f092c927d983e1dfdeda91583cbebe8dbb
Kirill <kirill.novikov@gmail.com>
2022-07-08 22:43:28 +0300

    add solution for task 7

---
+++ SolutionTask7/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите трехзначное число: ");
+string? inputLine = Console.ReadLine();
+
+if (inputLine != null)
+{
+    int inputNum = int.Parse(inputLine);
+    //Console.WriteLine(inputNum % 10);
+    Console.WriteLine(inputLine[inputLine.Length - 1]);
+}
