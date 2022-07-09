3d5a96aa159465fe6a1043ad13e295711de5d82e
Kirill <kirill.novikov@gmail.com>
2022-07-09 15:56:06 +0300

    add try/catch, comments and Math.Max for the solution for the homework task 4

--- SolutionHomeWork4/Program.cs
+++ SolutionHomeWork4/Program.cs
@@ -1,22 +1,29 @@
-﻿Console.WriteLine("Введите три числа:");
+﻿//-------------------------------- Решение задачи 4 ------------------------------------------------------
+//Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+//--------------------------------------------------------------------------------------------------------
+Console.WriteLine("Введите три числа:");
+//Считываем данные с консоли
 string? inputOne = Console.ReadLine();
 string? inputTwo = Console.ReadLine();
 string? inputThree = Console.ReadLine();
-
+//Проверяем, чтобы данные были не пустыми
 if (inputOne != null && inputTwo != null && inputThree != null)
 {
-    int max = int.Parse(inputOne);
+    try {
+    //Парсим первое число
+    int numOne = int.Parse(inputOne);
+    //Парсим второе число
     int numTwo = int.Parse(inputTwo);
+    //Парсим третье число
     int numThree = int.Parse(inputThree);
     Console.Write("Max = ");
-    if(numTwo > max) 
-    {
-        max = numTwo;
+    //Используем Math.max для определения максимаольного числа и выводим егоv в консоль
+    Console.Write(Math.Max(numThree, Math.Max(numOne, numTwo)));
     }
-    if(numThree > max) 
+    catch(Exception e)
     {
-        max = numThree;
+        // Выводим сообщение об ошибке
+        Console.WriteLine("Возникло исключение: " + e.Message);
+        Console.WriteLine("Попробуйте запустить программу еще раз.");        
     }
-
-    Console.Write(max);
 }
