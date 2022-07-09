fe4537755e0454d84735cc0553f5f8b74b25a202
Kirill <kirill.novikov@gmail.com>
2022-07-09 15:44:29 +0300

    add comments to the solution of the homework task 2

--- SolutionHomeWork2/Program.cs
+++ SolutionHomeWork2/Program.cs
@@ -1,10 +1,15 @@
-﻿Console.WriteLine("Введите два числа:");
+﻿//-------------------------------- Решение задачи 2 ------------------------------------------------------
+//Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+//--------------------------------------------------------------------------------------------------------
+Console.WriteLine("Введите два числа:");
+//Считываем данные с консоли
 string? inputOne = Console.ReadLine();
 string? inputTwo = Console.ReadLine();
-
+//Проверяем, чтобы данные были не пустыми
 if (inputOne != null && inputTwo != null)
 {
     try {
+        //Сравниваем два числа и выводим наибольшее
         Console.WriteLine("Max = " + (int.Parse(inputOne) > int.Parse(inputTwo) ? int.Parse(inputOne) : int.Parse(inputTwo)));
         // if(numOne > numTwo) 
         // {
@@ -17,6 +22,7 @@ if (inputOne != null && inputTwo != null)
     }
     catch(Exception e)
     {
+        // Выводим сообщение об ошибке
         Console.WriteLine("Возникло исключение: " + e.Message);
         Console.WriteLine("Попробуйте запустить программу еще раз");
     }
