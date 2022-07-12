1fc78bb46dc79d04d6ae3b427a95e31e11d2c95d
Kirill <kirill.novikov@gmial.com>
2022-07-12 19:50:59 +0300

    rename folders in sequence order

--- SolutionTask3/Program.cs
+++ SolutionTask3/Program.cs
@@ -1,23 +1,35 @@
-﻿Console.Write("Введите номер : ");
+﻿Console.Write("Введите номер дня недели от 1 до 7: ");
 string? inputLine = Console.ReadLine();
+string[] weekDays = new string[7] {"Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"};
 
-if (inputLine != null)
+if (inputLine != null) 
 {
-    int inputNum = int.Parse(inputLine);
-    int i = 1;
-    //string outputString = "";
-    // for (int i = - inputNum; i < inputNum; i++) 
-    // {
-    //     outputString = outputString + i + ", ";
-    // }
-    // outputString = outputString + inputNum;
-    // Console.WriteLine(outputString);
-
-    string outputString = "0";
-    while (i <= inputNum)
-    {
-        outputString = -i + ", " + outputString + ", " + i;
-        i++;
+    int weekNum = int.Parse(inputLine);
+    if (weekNum > 0 && weekNum < 7) {
+        //Console.Write("Введенный день недели: ");
+        //Console.Write(weekDays[weekNum - 1]);
+        switch(weekNum - 1) {
+            case 0:
+                Console.Write("Введенный день недели: Понедельник");
+            break;
+            case 1:
+                Console.Write("Введенный день недели: Вторник");
+            break;
+            case 2:
+                Console.Write("Введенный день недели: Среда");
+            break;
+            case 3:
+                Console.Write("Введенный день недели: Четверг");
+            break;
+            case 4:
+                Console.Write("Введенный день недели: Пятница");
+            break;
+            case 5:
+                Console.Write("Введенный день недели: Суббота");
+            break;
+            case 6:
+                Console.Write("Введенный день недели: Воскресенье");
+            break;                                                                        
+        }
     }
-    Console.WriteLine(outputString);
 }
---
+++ SolutionTask5/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Write("Введите номер : ");
+string? inputLine = Console.ReadLine();
+
+if (inputLine != null)
+{
+    int inputNum = int.Parse(inputLine);
+    int i = 1;
+    //string outputString = "";
+    // for (int i = - inputNum; i < inputNum; i++) 
+    // {
+    //     outputString = outputString + i + ", ";
+    // }
+    // outputString = outputString + inputNum;
+    // Console.WriteLine(outputString);
+
+    string outputString = "0";
+    while (i <= inputNum)
+    {
+        outputString = -i + ", " + outputString + ", " + i;
+        i++;
+    }
+    Console.WriteLine(outputString);
+}
