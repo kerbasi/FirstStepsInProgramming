db788bcc2f7ffcc8b04577c1785933b761c73748
Kirill <kirill.novikov@gmial.com>
2022-07-15 00:08:13 +0300

    add new algorythms to homework task 13

--- SolutionHomeWork13/Program.cs
+++ SolutionHomeWork13/Program.cs
@@ -2,6 +2,8 @@
 //Напишите программу, которая выводит третью цифру заданного числа
 // или сообщает, что третьей цифры нет.
 //----------------------------------------------------------------
+using System.Diagnostics;
+Stopwatch timer1 = new Stopwatch();
 Console.WriteLine("Введите любое число:");
 
 try {
@@ -12,16 +14,81 @@ try {
         //выводим в консоль что число меньше трехзначного
         Console.WriteLine("В заданном числе меньше трех цифр");
     } else {
-        //пока число больше трехзначного делим на 10
-        while (number > 999) {
-            number /= 10;
-        }
-        //выводим в консоль третью цифру заданнного числа
-        Console.WriteLine("Третья цифра заданного числа: " + (number % 10));
+
+
+        //вывод решения способо деления через while
+        timer1.Start();
+        Console.WriteLine("Третья цифра заданного числа полученная через деление на 10 в while: " + (thirdDigitDiv(number)));
+        timer1.Stop();
+        Console.WriteLine("Затраченное время на решение: " + timer1.Elapsed);
+        timer1.Reset();
+        timer1.Start();
+        Console.WriteLine("Третья цифра заданного числа полученная через if else: " + (thirdDigitIf(number)));
+        timer1.Stop();
+        Console.WriteLine("Затраченное время на решение: " + timer1.Elapsed);
+        timer1.Reset();
+        timer1.Start();
+        Console.WriteLine("Третья цифра заданного числа полученная через Log10: " + (thirdDigitLog(number)));
+        timer1.Stop();
+        Console.WriteLine("Затраченное время на решение: " + timer1.Elapsed);        
     }
 
 } catch(Exception e) {
     // Выводим сообщение об ошибке
     Console.WriteLine("Возникло исключение: " + e.Message);
     Console.WriteLine("Попробуйте запустить программу еще раз.");    
+}
+
+int thirdDigitDiv(int num){
+        //пока число больше трехзначного делим на 10
+        while (num > 999) {
+            num /= 10;
+        }
+        return num % 10;
+}
+
+int thirdDigitIf(int num){
+    int digitsNum;
+    //7 
+    if(num >= 1000000) {
+        //9
+        if (num >= 100000000) {
+            //10
+            if (num >= 1000000000) {
+                digitsNum = 10;
+            } else {
+                digitsNum = 9;
+            }
+        } else {
+            //8
+            if (num >= 10000000) {
+                digitsNum = 8;
+            } else {
+                digitsNum = 7;
+            }
+        }
+    } else {
+        //5
+        if(num >= 10000) {
+            //6
+            if (num >= 100000) {
+                digitsNum = 6;
+            } else {
+                digitsNum = 5;
+            } 
+        } else {
+            //4
+            if (num >= 1000) {
+                digitsNum = 4;
+            } else {
+                digitsNum = 3;
+            }
+        }
+    }
+    return num / (int)Math.Pow(10, digitsNum - 3) % 10;
+}
+
+int thirdDigitLog(int num) {
+        int digitsNum = num == 0 ? 1 : 1 + (int)Math.Log10(num);
+        return num / (int)Math.Pow(10, digitsNum - 3) % 10;
 }
