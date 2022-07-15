7d2583d7aad29419d5d0630df31baf60c818a5b3
Kirill <kirill.novikov@gmial.com>
2022-07-15 21:03:14 +0300

    add solution for task 18

---
+++ SolutionTask18/Program.cs
@@ -0,0 +1,14 @@
+﻿void printAnswer(int number) {
+    if (number == 1) Console.WriteLine("Допустимо X>0 Y>0");
+    if (number == 2) Console.WriteLine("Допустимо X<0 Y>0");
+    if (number == 3) Console.WriteLine("Допустимо X<0 Y<0");
+    if (number == 4) Console.WriteLine("Допустимо X>0 Y<0");
+}
+
+string? inputLine = Console.ReadLine();
+
+if (inputLine != null) 
+{
+    int inputNumber = int.Parse(inputLine);
+    printAnswer(inputNumber);
+}
