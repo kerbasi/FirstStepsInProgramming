7fb63b0df49b8da1dddfd364152ddeec83c697ce
Kirill <kirill.novikov@gmial.com>
2022-07-15 22:50:45 +0300

    rename folder 21 to 22

--- SolutionTask22/Program.cs
+++ SolutionTask22/Program.cs
@@ -1,5 +1,5 @@
 ﻿List<int> listGen(int number) {
-    List<int> numbers = new List();
+    List<int> numbers = new List<int>();
     for(int i = 1; i <= number; i++){
         numbers.Add(i);
     }
@@ -13,6 +13,12 @@ if (inputLine != null)
     int inputNumber = int.Parse(inputLine);
     List<int> nums = listGen(inputNumber);
     foreach (var i in nums) {
-        Console.WriteLine(Math.Pow(i, 2));
+        Console.Write(i);
+        Console.Write(" ");
+    }  
+    Console.WriteLine();
+    foreach (var i in nums) {
+        Console.Write(Math.Pow(i, 2));
+        Console.Write(" ");
     }
 }
