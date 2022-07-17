85c64cfcccd9b0c8f19dc4a227f9c7e8242c1627
Kirill <kirill.novikov@gmial.com>
2022-07-18 00:17:41 +0300

    fix solution for task 18

--- SolutionTask18/Program.cs
+++ SolutionTask18/Program.cs
@@ -1,9 +1,8 @@
-﻿List<int> listGen(int number) {
-    List<int> numbers = new List<int>();
-    for(int i = 1; i <= number; i++){
-        numbers.Add(i);
-    }
-    return numbers;
+﻿void printAnswer(int number) {
+    if (number == 1) Console.WriteLine("Допустимо X>0 Y>0");
+    if (number == 2) Console.WriteLine("Допустимо X<0 Y>0");
+    if (number == 3) Console.WriteLine("Допустимо X<0 Y<0");
+    if (number == 4) Console.WriteLine("Допустимо X>0 Y<0");
 }
 
 string? inputLine = Console.ReadLine();
@@ -11,14 +10,5 @@ string? inputLine = Console.ReadLine();
 if (inputLine != null) 
 {
     int inputNumber = int.Parse(inputLine);
-    List<int> nums = listGen(inputNumber);
-    foreach (var i in nums) {
-        Console.Write(i);
-        Console.Write(" ");
-    }  
-    Console.WriteLine();
-    foreach (var i in nums) {
-        Console.Write(Math.Pow(i, 2));
-        Console.Write(" ");
-    }
+    printAnswer(inputNumber);
 }
