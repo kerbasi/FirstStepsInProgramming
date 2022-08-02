c9e0801d935d1dd8e25b807a29e93f7e363a6f50
Kirill <kirill.novikov@gmial.com>
2022-08-02 20:44:26 +0300

    add solution for task 24

---
+++ SolutionTask24/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Введите число: ");
+string? inputLine = Console.ReadLine();
+int inputNum = int.Parse(inputLine);
+
+Console.WriteLine(sumNums(inputNum));
+Console.WriteLine(sumNums2(inputNum));
+
+int sumNums(int num)
+{
+    int sum = 0;
+    for (int i = 1; i <= inputNum; i++)
+    {
+        sum += i;
+    }
+    return sum;
+}
+
+int sumNums2(int num) {
+    return ((num + 1) * num ) / 2;
+}
